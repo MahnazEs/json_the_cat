264b6dd10a60f40a936d08ffd91b4e61051a36f9
“MahnazEs” <esmaeilymahnaz@gmail.com>
2023-02-16 20:53:34 -0800

    refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,27 @@
 const request = require('request');
 
 
+const fetchBreedDescription = function (breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request('https://api.thecatapi.com/v1/breeds/search?q=siberian', (error, response, body) => {
-  if (error) {
-    console.log("error: ", error);
-  }
-  if (body) {
+  request(url, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+
     const data = JSON.parse(body);
-    console.log("information about breed Siberian: ", data);
-    console.log(typeof data);
-  } else {
-    console.log("data not found");
-  }
-});
+    const breed = data[0];
+
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback('Breed Not Found!!!!!');
+    }
+  });
+};
+
+
+
+
+
+module.exports = { fetchBreedDescription };
